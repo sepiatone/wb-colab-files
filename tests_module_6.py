ecce5d1d7325d4d6c35afc198994af7d9271bce3
aditya thomas <aditya.thomas@alum.mit.edu>
2025-06-25 20:08:59 +0530

    Update tests_module_6.py

--- tests_module_6.py
+++ tests_module_6.py
@@ -1,6 +1,47 @@
 # import part2_intro_to_mech_interp.tests as tests
-from .submit import test_submit
+# from .submit import test_submit
+import requests
+import os
+from dotenv import load_dotenv
+from hashlib import md5
 import torch as t
+
+
+SUPABASE_URL = "https://udnmlcykctnahtnzmmmx.supabase.co"
+
+def generate_hash(mid, eid, iid, uid):
+    return md5("-".join(map(str, [mid, eid, iid, uid])).encode()).hexdigest()
+
+def test_submit(iid, is_checked=True, mid=4, eid=1):
+    load_dotenv()
+    jwt_token = os.getenv("JWT")
+    uid = os.getenv("UID")
+    supabase_key = os.getenv("SUPABASE_KEY")
+    if not jwt_token or not uid:
+        print("❌ Please login first.")
+        return
+    else:
+        auth_headers = {
+            "apikey": supabase_key,
+            "Authorization": f"Bearer {jwt_token}",
+            "Content-Type": "application/json",
+            "Prefer": "resolution=merge-duplicates"
+        }
+
+        payload = {
+            "id": generate_hash(mid, eid, iid, uid),
+            "module_id": mid,
+            "exercise_id": eid,
+            "item_id": iid,
+            "user_id": uid,
+            "is_checked": is_checked,
+        }
+
+        url = f"{SUPABASE_URL}/rest/v1/item_completion"
+        response = requests.post(url, json=payload, headers=auth_headers)
+        
+        if response.status_code not in [200, 201]:
+            print("Insert failed:", response.json())
 
 
 def tester():
