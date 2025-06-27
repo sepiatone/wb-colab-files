8a670f0a70895a6fc002e8d6ac4f4e43f259b459
aditya thomas <aditya.thomas@alum.mit.edu>
2025-06-27 10:36:05 +0530

    update wb_utils.py
    - add imshow()

--- wb_utils.py
+++ wb_utils.py
@@ -33,3 +33,92 @@ def plot_loss_difference(log_probs, rep_str, seq_len, filename: str | None = Non
     fig.show()
     if filename is not None:
         fig.write_html(filename)
+
+
+update_layout_set = {
+    "xaxis_range",
+    "yaxis_range",
+    "hovermode",
+    "xaxis_title",
+    "yaxis_title",
+    "colorbar",
+    "colorscale",
+    "coloraxis",
+    "title_x",
+    "bargap",
+    "bargroupgap",
+    "xaxis_tickformat",
+    "yaxis_tickformat",
+    "title_y",
+    "legend_title_text",
+    "xaxis_showgrid",
+    "xaxis_gridwidth",
+    "xaxis_gridcolor",
+    "yaxis_showgrid",
+    "yaxis_gridwidth",
+    "yaxis_gridcolor",
+    "showlegend",
+    "xaxis_tickmode",
+    "yaxis_tickmode",
+    "margin",
+    "xaxis_visible",
+    "yaxis_visible",
+    "bargap",
+    "bargroupgap",
+    "coloraxis_showscale",
+    "xaxis_tickangle",
+    "yaxis_scaleanchor",
+    "xaxis_tickfont",
+    "yaxis_tickfont",
+}
+
+
+def imshow(tensor: torch.Tensor, renderer=None, **kwargs):
+    kwargs_post = {k: v for k, v in kwargs.items() if k in update_layout_set}
+    kwargs_pre = {k: v for k, v in kwargs.items() if k not in update_layout_set}
+    if ("size" in kwargs_pre) or ("shape" in kwargs_pre):
+        size = kwargs_pre.pop("size", None) or kwargs_pre.pop("shape", None)
+        kwargs_pre["height"], kwargs_pre["width"] = size  # type: ignore
+    facet_labels = kwargs_pre.pop("facet_labels", None)
+    border = kwargs_pre.pop("border", False)
+    return_fig = kwargs_pre.pop("return_fig", False)
+    text = kwargs_pre.pop("text", None)
+    xaxis_tickangle = kwargs_post.pop("xaxis_tickangle", None)
+    # xaxis_tickfont = kwargs_post.pop("xaxis_tickangle", None)
+    static = kwargs_pre.pop("static", False)
+    if "color_continuous_scale" not in kwargs_pre:
+        kwargs_pre["color_continuous_scale"] = "RdBu"
+    if "color_continuous_midpoint" not in kwargs_pre:
+        kwargs_pre["color_continuous_midpoint"] = 0.0
+    if "margin" in kwargs_post and isinstance(kwargs_post["margin"], int):
+        kwargs_post["margin"] = dict.fromkeys(list("tblr"), kwargs_post["margin"])
+    fig = px.imshow(to_numpy(tensor), **kwargs_pre).update_layout(**kwargs_post)
+    if facet_labels:
+        # Weird thing where facet col wrap means labels are in wrong order
+        if "facet_col_wrap" in kwargs_pre:
+            facet_labels = reorder_list_in_plotly_way(facet_labels, kwargs_pre["facet_col_wrap"])
+        for i, label in enumerate(facet_labels):
+            fig.layout.annotations[i]["text"] = label  # type: ignore
+    if border:
+        fig.update_xaxes(showline=True, linewidth=1, linecolor="black", mirror=True)
+        fig.update_yaxes(showline=True, linewidth=1, linecolor="black", mirror=True)
+    if text:
+        if tensor.ndim == 2:
+            # if 2D, then we assume text is a list of lists of strings
+            assert isinstance(text[0], list)
+            assert isinstance(text[0][0], str)
+            text = [text]
+        else:
+            # if 3D, then text is either repeated for each facet, or different
+            assert isinstance(text[0], list)
+            if isinstance(text[0][0], str):
+                text = [text for _ in range(len(fig.data))]
+        for i, _text in enumerate(text):
+            fig.data[i].update(text=_text, texttemplate="%{text}", textfont={"size": 12})
+    # Very hacky way of fixing the fact that updating layout with xaxis_* only applies to first facet by default
+    if xaxis_tickangle is not None:
+        n_facets = 1 if tensor.ndim == 2 else tensor.shape[0]
+        for i in range(1, 1 + n_facets):
+            xaxis_name = "xaxis" if i == 1 else f"xaxis{i}"
+            fig.layout[xaxis_name]["tickangle"] = xaxis_tickangle  # type: ignore
+    return fig if return_fig else fig.show(renderer=renderer, config={"staticPlot": static})
